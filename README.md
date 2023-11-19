5fdda1c5d72e16050f79e565b64d5c1a01e24f4d
Kaan Demir <146492433+wkaandemir@users.noreply.github.com>
2023-11-19 19:42:31 +0300

    .

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# healthliteracywebapp
- 
+# Health Literacy Test - Deployment
+![Python 3.6](https://img.shields.io/badge/Python-3.6-brightgreen.svg) ![scikit-learnn](https://img.shields.io/badge/Library-Scikit_Learn-orange.svg)
+
+• This repository consists of files required to deploy a ___Machine Learning Web App___ created with ___Flask___ on ___Heroku___ platform.
+
+• If you wish to see the deployed model, please click on the following link.:<br />
+Deployed at: https://healthliteracywebapp-de419696e359.herokuapp.com/
+
+• If you are looking for the code, click on the link provided below for it.:<br />
+Link:  https://github.com/wkaandemir/health.literacy.webapp
---
+++ app.py
@@ -0,0 +1,42 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the Random Forest CLassifier model
+filename = 'ModelK-Nearest_Neighbors.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    age = float(request.form['age'])      
+    gender = float(request.form['gender'])
+    classs = float(request.form['classs'])
+    mother = float(request.form['mother'])
+    father = float(request.form['father']) 
+    live = float(request.form['live'])          
+    news = float(request.form['news'])      
+    health = float(request.form['health'])   
+    economy = float(request.form['economy'])        
+
+    input_data = np.array([age, gender, classs, mother, father, live, news, health, economy]).reshape(1, -1)
+
+    prediction = model.predict(input_data)
+
+    if prediction == 1:
+        return render_template('result.html', prediction_text="Your Health Literacy is High")
+    else:
+        return render_template('result.html', prediction_text="Your Health Literacy is Low")
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
