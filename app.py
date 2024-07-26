5b9041d770e33936a493fe801f060745a59f84dd
Kaan Demir <146492433+wkaandemir@users.noreply.github.com>
2024-07-26 14:43:49 +0300

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-# Load the Random Forest CLassifier model
+# Load the Random Forest Classifier model
 filename = 'ModelK-Nearest_Neighbors.pkl'
 model = pickle.load(open(filename, 'rb'))
 
@@ -11,19 +11,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    age = float(request.form['age'])      
+    age = float(request.form['age'])
     gender = float(request.form['gender'])
     classs = float(request.form['classs'])
     mother = float(request.form['mother'])
-    father = float(request.form['father']) 
-    live = float(request.form['live'])          
-    news = float(request.form['news'])      
-    health = float(request.form['health'])   
-    economy = float(request.form['economy'])        
+    father = float(request.form['father'])
+    live = float(request.form['live'])
+    news = float(request.form['news'])
+    health = float(request.form['health'])
+    economy = float(request.form['economy'])
 
     input_data = np.array([age, gender, classs, mother, father, live, news, health, economy]).reshape(1, -1)
 
@@ -34,9 +34,5 @@ def predict():
     else:
         return render_template('result.html', prediction_text="Your Health Literacy is Low")
 
-
-
-
-
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
